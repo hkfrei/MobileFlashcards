ded751e492ab6cd013510aca01ca042985dccd53
Hanskaspar Frei <hkfrei@karten-werk.ch>
2018-05-02 09:59:28 +0200

    feat: Add Redux and proper styling to the add desk view

--- components/CreateDeck.js
+++ components/CreateDeck.js
@@ -1,13 +1,42 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
-const CreateDeck = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>What is the title of your deck?</Text>
-      <TextInput autoFocus clearButtonMode="always" style={styles.deckName} />
-    </View>
-  );
-};
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Button
+} from "react-native";
+
+class CreateDeck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deckName: ""
+    };
+  }
+
+  addDeck() {
+    console.log("add deck");
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          placeholder="PLEASE ENTER A DECK NAME"
+          autoFocus
+          clearButtonMode="always"
+          style={styles.deckName}
+          onChangeText={deckName => this.setState({ deckName })}
+          value={this.state.deckName}
+        />
+        <TouchableOpacity style={styles.btnDefault} onPress={this.addDeck}>
+          <Text style={styles.btnText}>ADD DECK</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 export default CreateDeck;
 
@@ -18,17 +47,23 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   heading: {
-    fontSize: 20,
-    fontWeight: "bold"
+    fontSize: 20
   },
   deckName: {
-    borderColor: "black",
-    borderRadius: 10,
-    borderWidth: 1,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
     height: 40,
-    paddingLeft: 10,
-    paddingRight: 10,
     marginTop: 20,
     width: 300
+  },
+  btnDefault: {
+    backgroundColor: "#2089dc",
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 50
+  },
+  btnText: {
+    color: "white",
+    fontWeight: "bold"
   }
 });
